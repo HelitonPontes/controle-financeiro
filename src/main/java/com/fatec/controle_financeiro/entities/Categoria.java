b23802bbfbcadbbb3bd9db3d773746d67fc98d62
HelitonPontes <powerheliton@hotmail.com>
2024-09-12 22:25:21 -0300

    Aula 12/099/2024

---
+++ src/main/java/com/fatec/controle_financeiro/entities/Categoria.java
@@ -0,0 +1,9 @@
+package com.fatec.controle_financeiro.entities;
+
+public class Categoria {
+
+    private int id;
+    private String Descricao;
+    
+    
+}
---
+++ src/main/java/com/fatec/controle_financeiro/entities/Produto.java
@@ -0,0 +1,12 @@
+package com.fatec.controle_financeiro.entities;
+
+public class Produto {
+
+    private int id;
+    private String nome;
+    private double valorVenda;
+    private double valorCompra;
+    private int qtdEstoque;
+    private Categoria categoria;
+    
+}
